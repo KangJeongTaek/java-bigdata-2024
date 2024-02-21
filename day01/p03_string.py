7da6e6c033d391e5ab9813916dc04d9ac3093b5b
KangJeongTaek <dfgddjd@naver.com>
2024-02-21 15:38:37 +0900

    3:38

--- day01/p03_string.py
+++ day01/p03_string.py
@@ -53,7 +53,31 @@ print(ascii('안'), ascii('녕'),ascii('하'),ascii('세'),ascii('요'))
 # 기존 문자열로 새로운 문자열을 만들 때는 슬라이싱, 다른 문자열로 대체
 print(cp[0:7], 'long', cp[17:])
 
+# 다른 언어에는 없는 마이너스 슬라이싱
 print(cp[-1])
 print(cp[-6:-1])
 print(cp[-6:])
-print(cp[-11:-7])
+print(cp[-11:-7]) #-로 슬라이싱 할 때도 : 뒤에는 +1을 해줘야 한다
+
+## 문자열 포매팅 (format string)
+##파이썬에서는 %d,%s,%c 등 포맷스티링용 연산자 사용 빈도가 낮다.
+
+temp = 21
+print('현재 온도는 %d도 입니다.'%temp)
+temp = 17
+print('현재 온도는 %d도 입니다.'%temp)
+
+## format 함수로 포맷팅(가장 일반적)
+
+print("현재 온도는 {0}도 입니다." .format(temp))
+
+## 날짜를 포매승로 만들 때
+month = 2
+day = 21
+hour = 15
+mins = 18
+print('오늘은 {0:02d}월 {1:02d}일 {2:02d}시 {3:02d}분입니다.'.format(month,day,hour,mins))
+
+income = 6000000 # 육백만원
+
+print('이번 달 매출액은 {0:,d}원 입니다.'.format(income))
