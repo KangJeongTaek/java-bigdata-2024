c0873b4e1f03072f655125e76e24b59cb81cad59
KangJeongTaek <dfgddjd@naver.com>
2024-02-22 16:18:11 +0900

    Update p13_starPrint.py

--- day02/p13_starPrint.py
+++ day02/p13_starPrint.py
@@ -28,3 +28,34 @@ for i in range(1,6):
     for j in range(6-i,6):
             print('*', end='')
     print()
+
+for i in range(1,6):
+    for j in range(i,6):
+            print(' ', end ='')
+    for k in range(6-i,6):
+            print(' ', end ='')
+            print('*',end='')
+    print()
+
+
+b = 10
+i = 0
+while True:
+    i += 1
+    if i> 5 : break
+    print('*'*i)
+
+for i in range(1,101):
+    print(i)
+
+A = {70,60,55,75,95,90,80,80,85,100}
+total = 0
+for score in A:
+    total += score
+avg = total// len(A)
+
+print(avg)
+
+numbers = [1,2,3,4,5]
+result = [b for m in numbers if m % 2 != 0]
+print(result)
