c4abeed47d558c61329b41629a5c2e8e588a74d1
KangJeongTaek <dfgddjd@naver.com>
2024-02-22 13:29:19 +0900

    Update p09_ifCondition.py

--- day02/p09_ifCondition.py
+++ day02/p09_ifCondition.py
@@ -7,6 +7,15 @@ if(money>=5000):
     print("택시")
 elif(money<5000 and money>=2500):
     print('기사는 홈플러스 앞까지만 가주세요')
-elif()
 else:
     print("걸어")
+
+
+marks = [90, 25, 67, 45, 80]
+
+number = 0 
+for mark in marks: 
+    number = number +1 
+    if mark < 60:
+        continue 
+    print(f"{number}번 학생 축하합니다. {mark:.1f}의 점수로 합격입니다. " )    
