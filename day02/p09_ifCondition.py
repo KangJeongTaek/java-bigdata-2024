68665acb8a7594fdd9ab6f68d3107b3fac859bc9
KangJeongTaek <dfgddjd@naver.com>
2024-02-22 12:33:45 +0900

    하이

---
+++ day02/p09_ifCondition.py
@@ -0,0 +1,12 @@
+# file : p09+ifCondition
+# desc : if 제어문
+money = 5000
+haveMoney = True
+if(money>=5000):
+    #indentation(들여쓰기) == space 4
+    print("택시")
+elif(money<5000 and money>=2500):
+    print('기사는 홈플러스 앞까지만 가주세요')
+elif()
+else:
+    print("걸어")
