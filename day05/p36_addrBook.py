d3ded3feca2c4619db2d2f62b7784a1ce45a9a99
KangJeongTaek <dfgddjd@naver.com>
2024-02-27 12:37:12 +0900

    점심 시간

--- day05/p36_addrBook.py
+++ day05/p36_addrBook.py
@@ -16,6 +16,16 @@ class Contact:
                 f'주  소 : {self.__addr}')
         return strRes
     
+    def getInfo(self):
+        return self.__name,self.__phoneNumber,self.__eMail,self.__addr
+    
+    # 연락처 여부 확인
+    def isNameExist(self,name):
+        if self.__name == name:
+            return True
+        else:
+            return False
+    
 
 def setContact(): # 사용자 입력으로 주소록 받기 함수
     name,phoneNumber,eMail,addr = input('정보 입력(이름,휴대폰,이메일,주소[/])> ').split('/')
@@ -34,8 +44,22 @@ def clearConsole():
     os.system(command)
 
 def getContacts(lst): #리스트를 받아서 출력하는 함수
-    for item in lst:
+    for i, item in enumerate(lst):
+        print(f'{i+1} ==========> ')
         print(item)
+
+def delContact(lst, name): #연락처 삭제함수
+    for i, item in enumerate(lst):
+        if item.isNameExist(name):
+            del lst[i]
+
+
+def saveContact(lst): #연락처 저장 함수
+    with open('./contacts.txt',mode='w',encoding='utf-8') as fp:
+        for item in lst:
+            name,phoneNumber,eMail,addr = item.getInfo()
+            fp.write(f'{name}/{phoneNumber}/{eMail}/{addr}\n')
+    
 
 def displayMenu():
     menu = ('주소록 프로그램\n'
@@ -58,14 +82,21 @@ def run():
             clearConsole()
             contact = setContact()
             listContact.append(contact)
-            input() # 아무키를 누를 때까지 정지
+            input('입력 성공!') # 엔터를 누를 때까지 정지
             clearConsole()
             
-        elif selMenu == 2:
+        elif selMenu == 2: #연락처 출력
             clearConsole()
             getContacts(listContact)
-            input(); clearConsole()
+            input('출력 성공!'); clearConsole()
+
+        elif selMenu == 3: #연락처 삭제
+            clearConsole()
+            name = input('삭제할 이름 입력 : ')
+            delContact(listContact,name)
+            input('삭제 성공!');clearConsole()
         elif selMenu == 4: # 종료
+            saveContact(listContact)
             break
         else:
             clearConsole()
