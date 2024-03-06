5523c6164509b6fb77973b0e9d826d5412acf8f2
KangJeongTaek <dfgddjd@naver.com>
2024-03-06 23:27:56 +0900

    커밋

--- day10/p74_note.py
+++ day10/p74_note.py
@@ -1,22 +1,33 @@
 import sys
 from PyQt5 import QtGui,QtCore,QtWidgets,uic
-from PyQt5.QtCore import Qt,QThread,pyqtSignal
+from PyQt5.QtCore import Qt,QThread,pyqtSignal, pyqtSlot
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-import keyboard
 
 
 class QtBackGroundWorker(QThread):
-    initUiSignal = pyqtSignal(bool)
-
+    textChanged = pyqtSignal()
     def __init__(self, parent):
         super().__init__(parent)
+        self.parent = parent
 
 
-    def run(self):
-        pass #키보드를 입력받는다면 True를 리턴하라
+    def run(self): # 저장된 값과 현재의 텍스트가 다르면 *를 붙이자
+        previousTxt = self.parent.plainTextEdit.toPlainText()
+        self.working = True
 
-class WinApp(QMainWindow): # QWidget이 아님
+        while self.working:
+            currentTxt  = self.parent.plainTextEdit.toPlainText()
+            if previousTxt != currentTxt:
+                self.textChanged.emit()
+
+            previousTxt = currentTxt
+            self.usleep(100*1000)
+    def stop(self):
+        self.working = False
+        self.quit()
+
+class WinApp(QMainWindow): 
 
 
 
@@ -33,6 +44,10 @@ class WinApp(QMainWindow): # QWidget이 아님
 
 
         self.show()
+        self.worker_thread = QtBackGroundWorker(self)
+        self.worker_thread.textChanged.connect(self.updateWindowTitle)
+        self.worker_thread.start()
+        
 
 
     def initSignal(self):
@@ -43,6 +58,11 @@ class WinApp(QMainWindow): # QWidget이 아님
         self.action_E_Exit.triggered.connect(self.action_Exit_clicked)
         
 
+    def updateWindowTitle(self):
+        title = self.windowTitle()
+        if '*' not in title:
+            self.setWindowTitle(f'{title}*')
+
 
     def actionOpen_Clicked(self):
         text = QFileDialog.getOpenFileName(self,'텍스트 열기','','Txt file(*.txt)')
@@ -52,21 +72,31 @@ class WinApp(QMainWindow): # QWidget이 아님
             self.plainTextEdit.setPlainText(open(textPath,encoding='UTf-8').read())
             global title
             title = textPath.split('/')[-1]
+            if '*' in title:
+                title = title.rstrip('*')
             self.setWindowTitle(title)
 
 
     def actionSave_Clicked(self):
         try:
-            with open(textPath,mode='w',encoding='UTF-8') as f:
-                f.write(self.plainTextEdit.toPlainText())
+            if filePath:
+                with open(filePath,mode='w',encoding='UTF-8') as f:
+                    f.write(self.plainTextEdit.toPlainText())
+                    self.setWindowTitle(self.windowTitle().rstrip('*'))
+            else:
+                with open(textPath,mode='w',encoding='UTF-8') as f:
+                    f.write(self.plainTextEdit.toPlainText())
+                    self.setWindowTitle(self.windowTitle().rstrip('*'))
         except NameError:
             self.actionSave_as_Clicked()
 
     def actionSave_as_Clicked(self):
+        global filePath
         filePath, _ = QFileDialog.getSaveFileName(self,'텍스트 저장','','Txt file(*.txt)')
         try:
             with open(filePath,'w',encoding='UTF-8') as f:
                 f.write(self.plainTextEdit.toPlainText())
+                self.setWindowTitle(filePath.split('/')[-1])
         except FileNotFoundError:
             pass
             
@@ -77,19 +107,24 @@ class WinApp(QMainWindow): # QWidget이 아님
 
     def action_Exit_clicked(self):
         re = QMessageBox.question(self,'종료확인','종료하시겠습니까?',QMessageBox.Yes|QMessageBox.No)
-        if re == QMessageBox.Yes: #진짜 닫음
+        if re == QMessageBox.Yes:
+            self.worker_thread.working = False
             exit(0)
         else:
             QMessageBox.about(self,'취소','종료하지 않습니다.')
 
 
-    def closeEvent(self, QCloseEvent): # 오버라이드(재정의)
+    def closeEvent(self, QCloseEvent): 
         re = QMessageBox.question(self,'종료확인','종료하시겠습니까?',QMessageBox.Yes|QMessageBox.No)
-        if re == QMessageBox.Yes: #진짜 닫음
+        if re == QMessageBox.Yes: 
+            self.worker_thread.working = False
             QCloseEvent.accept()
         else:
             QCloseEvent.ignore()
             QMessageBox.about(self,'취소','종료하지 않습니다.')
+
+
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
